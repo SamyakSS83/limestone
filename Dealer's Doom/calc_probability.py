97d06f2716bc76b48a05b1634339f62571121cf7
vanigupta262 <vani21226@gmail.com>
2025-03-22 17:44:06 +0530

    part 1

--- Dealer's Doom/calc_probability.py
+++ Dealer's Doom/calc_probability.py
@@ -15,15 +15,65 @@ def data_loader(path, table_idx, player_or_dealer):
     card = data[(f'table_{table_idx}', player_or_dealer, 'card')]
     return np.array([spy, card]).T
 
+# def bust_probability(dealer_cards):
+#     """
+#     dealer_cards: list -> integer series of player denoting value of cards observed upto this point
+
+#     Current body is random for now, change it accordingly
+    
+#     output: probability of going bust on this table
+#     """
+#     bust_count = 0
+#     total_count = 0
+#     n = len(dealer_cards)
+#     for i in range(n):
+#         index  = i
+#         total = 0
+
+#         while(total <= 16 and index < n):
+#             total += dealer_cards[index]
+#             index += 1
+#         if(total>21):
+#             bust_count += 1
+#         total_count += 1
+#     if total_count != 0:
+#         return bust_count/total_count
+#     else:   
+
+
+#         return 0
+    
 def bust_probability(dealer_cards):
     """
-    dealer_cards: list -> integer series of player denoting value of cards observed upto this point
-
-    Current body is random for now, change it accordingly
+    dealer_cards: list of integers representing card values (2-10, 10 for face cards, 11 for aces)
     
-    output: probability of going bust on this table
+    output: probability of dealer busting based on sequential hand simulation
     """
-    return 0
+    bust_count = 0
+    total_count = 0
+    index = 0
+    n = len(dealer_cards)
+    
+    while index < n:
+        total = 0
+        
+        while total <= 16 and index < n:
+            total += dealer_cards[index]
+            index += 1
+        if total > 21:
+            bust_count += 1
+        total_count += 1
+    
+    return bust_count / total_count if total_count > 0 else 0
 
-dealer_cards = [int(a[1]) for a in data_loader('train.csv', 0, 'dealer')]
-print(dealer_cards)
+
+data = data_loader("train.csv", 0, 'player')  
+spy_values = data[:, 0]
+card_values = data[:, 1]
+
+plt.scatter(spy_values, card_values, alpha=0.5)
+plt.xlabel('Spy Value')
+plt.ylabel('Card Value')
+plt.title('Spy Values vs Card Values')
+plt.grid(True)
+plt.show()
